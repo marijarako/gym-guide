ca57f4c024a02447d0d6f7c3aff65762697a1a5b
Marija Rakocevic <136706373+marijarako@users.noreply.github.com>
2026-02-24 19:50:21 +0100

    Update script.js

--- script.js
+++ script.js
@@ -7,3 +7,29 @@ function showPage(pageId) {
     document.getElementById(pageId).classList.add('active');
     window.scrollTo(0, 0);
 }
+function toggleExercise(header) {
+    const allContents = document.querySelectorAll(".exercise-content");
+
+    allContents.forEach(content => {
+        if (content !== header.nextElementSibling) {
+            content.style.maxHeight = null;
+            content.classList.remove("active");
+            const video = content.querySelector("video");
+            if (video) video.pause();
+        }
+    });
+
+    const content = header.nextElementSibling;
+
+    if (content.style.maxHeight) {
+        content.style.maxHeight = null;
+        content.classList.remove("active");
+        const video = content.querySelector("video");
+        if (video) video.pause();
+    } else {
+        content.classList.add("active");
+        content.style.maxHeight = content.scrollHeight + "px";
+        const video = content.querySelector("video");
+        if (video) video.play();
+    }
+}
